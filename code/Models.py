0557d8b4c42b9e5c52ec93bb85cefca045cc9c9b
Adriman32 <adriman32r@gmail.com>
2023-03-16 23:13:20 -0400

    Initial Upload
    Uploaded the current version of the implementation. Currently includes only the architecture of the VGG16 classifier and functions to facilitate training. Dataset can be downloaded at the link in the ReadMe. The network is too large for my personal computer so I will attempt running on Virginia Tech's ARC clusters. If training from scratch is not an option, transfer learning will be explored.

---
+++ code/Models.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from keras.models import Model
+from keras.layers import Conv2D, Activation, MaxPooling2D, Dense, Flatten, Dropout, Concatenate, Input
+from PIL import Image
+
+
+class VGG16:
+	def __init__(self):
+		self.model = self.create_model()
+		self.history = None
+		self.feature_blocks = []
+
+	def create_model(self):
+		input_layer = Input(shape=(224,224,3))
+		conv_layer_1 = Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding='same')(input_layer)
+		act_layer_1 = Activation(activation='relu')(conv_layer_1)
+		conv_layer_2 = Conv2D(filters=64, kernel_size=(3,3),padding='same')(act_layer_1)
+		act_layer_2 = Activation(activation='relu')(conv_layer_2)
+		max_pool_layer_1 = MaxPooling2D(pool_size=(2,2),strides=(2,2))(act_layer_2)
+
+		conv_layer_3 = Conv2D(filters=128,kernel_size=(3,3),padding='same')(max_pool_layer_1)
+		act_layer_3 = Activation(activation='relu')(conv_layer_3)
+		conv_layer_4 = Conv2D(filters=128,kernel_size=(3,3),padding='same')(act_layer_3)
+		act_layer_4 = Activation(activation='relu')(conv_layer_4)
+		max_pool_layer_2 = MaxPooling2D(pool_size=(2,2),strides=(2,2))(act_layer_4)
+
+		conv_layer_5 = Conv2D(filters=256,kernel_size=(3,3),padding='same')(max_pool_layer_2)
+		act_layer_5 = Activation(activation='relu')(conv_layer_5)
+		conv_layer_6 = Conv2D(filters=256,kernel_size=(3,3),padding='same')(act_layer_5)
+		act_layer_6 = Activation(activation='relu')(conv_layer_6)
+		conv_layer_7 = Conv2D(filters=256,kernel_size=(3,3),padding='same')(act_layer_6)
+		act_layer_7 = Activation(activation='relu')(conv_layer_7)
+		max_pool_layer_3 = MaxPooling2D(pool_size=(2,2),strides=(2,2))(act_layer_7)
+		
+		conv_layer_8 = Conv2D(filters=512,kernel_size=(3,3),padding='same')(max_pool_layer_3)
+		act_layer_8 = Activation(activation='relu')(conv_layer_8)
+		conv_layer_9 = Conv2D(filters=512,kernel_size=(3,3),padding='same')(act_layer_8)
+		act_layer_9 = Activation(activation='relu')(conv_layer_9)
+		conv_layer_10 = Conv2D(filters=512,kernel_size=(3,3),padding='same')(act_layer_9)
+		act_layer_10 = Activation(activation='relu')(conv_layer_10)
+		max_pool_layer_4 = MaxPooling2D(pool_size=(2,2),strides=(2,2))(act_layer_10)
+
+		conv_layer_11 = Conv2D(filters=512,kernel_size=(3,3),padding='same')(max_pool_layer_4)
+		act_layer_11 = Activation(activation='relu')(conv_layer_11)
+		conv_layer_12 = Conv2D(filters=512,kernel_size=(3,3),padding='same')(act_layer_11)
+		act_layer_12 = Activation(activation='relu')(conv_layer_12)
+		conv_layer_13 = Conv2D(filters=512,kernel_size=(3,3),padding='same')(act_layer_12)
+		act_layer_13 = Activation(activation='relu')(conv_layer_13)
+		max_pool_layer_5 = MaxPooling2D(pool_size=(2,2),strides=(2,2))(act_layer_13)
+
+		flatten_layer = Flatten()(max_pool_layer_5)
+		dense_layer_1 = Dense(units=4096)(flatten_layer)
+		act_layer_14 = Activation(activation='relu')(dense_layer_1)
+		dense_layer_2 = Dense(units=4096)(act_layer_14)
+		act_layer_15 = Activation(activation='relu')(dense_layer_2)
+		dense_layer_3 = Dense(units=4)(act_layer_15)
+		act_layer_16 = Activation(activation='softmax')(dense_layer_3)
+
+		self.model = Model(inputs=input_layer,outputs=act_layer_16)
+		return self.model
+
+	def compile(self,optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy']):
+		self.model.compile(optimizer=optimizer,loss=loss,metrics=metrics)
+		return self.model 
+
+	def train(self,x_train,y_train):
+		self.history = self.model.fit(x_train,y_train)
+		return self.history
+
+	def evaluate(self,x_test,y_test):
+		loss, accuracy = self.model.evaluate(x_test,y_test)
+		return loss, accuracy 
+
+	def predict(self,sample):
+		output = self.model.predict(sample)
+		return output
+
+	def summary(self):
+		model_summary = self.model.summary()
+		return model_summary
+
+	def save(self,model_name=""):
+		if(model_name == ""):
+			model_name = 'output_models/VGG16_' + str(round(self.history.history['accuracy'][0], 3)) + '.h5' 
+		self.model.save(model_name)
+		print("Model saved to " + model_name)
---
+++ code/main.py
@@ -0,0 +1,70 @@
+from Models import VGG16
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import tensorflow as tf
+import time
+
+def  load_data(path):
+    load_start = time.time()
+    img_list = []
+    label_list = []
+    for curr_class in os.listdir(path):
+        for patient_num in os.listdir(path+curr_class+'/'):
+            for filename in os.listdir(path+curr_class+'/'+patient_num+'/'):
+                parsed_name = filename.split('_')
+                if parsed_name[0] == 'OD' or parsed_name[0] == 'OS':
+                    for os_od_filename in os.listdir(path+curr_class+'/'+patient_num+'/'+filename+'/'):
+                        parsed_name = os_od_filename.split('_')
+                        img_label = parsed_name[1].split('.')[0]
+                        img = Image.open(path+curr_class+'/'+patient_num+'/'+filename+'/'+os_od_filename).resize((224,224))
+                        img_arr = np.asarray(img)
+                        img_arr = img_arr.astype('float32') / 255.0
+                        img_list.append(img_arr)
+                        label_list.append(img_label)
+                else:
+                    img_label = parsed_name[1].split('.')[0]
+                    img = Image.open(path+str(curr_class)+'/'+str(patient_num)+'/'+filename).resize((224,224))
+                    img_arr = np.asarray(img)
+                    img_arr = img_arr.astype('float32') / 255.0
+                    img_list.append(img_arr)
+                    label_list.append(img_label)
+
+    label_encoder = LabelEncoder()
+    onehot_encoder = OneHotEncoder(sparse=False)
+
+    label_list = label_encoder.fit_transform(label_list)
+    label_list = label_list.reshape(len(label_list), 1)
+    label_list = onehot_encoder.fit_transform(label_list)
+    print('Data loaded in %s minutes'%(round((time.time()-load_start)/60,4)))
+    return img_list,label_list
+
+
+if __name__ == '__main__': 
+    start_time = time.time()
+    # path = 'D:/Users/Adrian Ruvalcaba/Documents/School/2022-2023/Spring 2023/Independent Study/dataset/8kt969dhx6-1/temp_dataset/'
+    path = 'D:/Users/Adrian Ruvalcaba/Documents/School/2022-2023/Spring 2023/Independent Study/dataset/8kt969dhx6-1/NEH_UT_2021RetinalOCTDataset/NEH_UT_2021RetinalOCTDataset/'
+    img_array_list, img_labels = load_data(path)
+    x_train, x_test, y_train, y_test = train_test_split(img_array_list, img_labels,random_state=42)
+
+    x_train = tf.stack(x_train)
+    x_test = tf.stack(x_test)
+
+    my_model = VGG16()
+    my_model.compile()
+    print('Now Training Model...')
+    history = my_model.train(x_train,y_train)
+    print('Training Complete!')
+    print('Evaluating Model using Test Set')
+    model_loss, model_accuracy = my_model.evaluate(x_test, y_test)
+    print('Evaluation Complete!')
+    print("--------------------------------------------------------------")
+    print('Loss: %s\tAccuracy: %s'%(round(model_loss,4),round(model_accuracy,4)))
+    print("--------------------------------------------------------------")
+    my_model.save()
+    print('Total Run Time: %s minutes'%(round((time.time()-start_time)/60,4)))
